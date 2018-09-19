5da634e3912d31ebd1e9bd807f96f316d46f2a0b
SeongChan Lee <sclee@kodebox.io>
2018-09-19 15:10:10 +0900

    change flask/requests to aiohttp

--- proxy.py
+++ proxy.py
@@ -1,78 +1,80 @@
 #!/usr/bin/env python3
 import argparse
-from flask import Flask
-from flask import request
-import requests
+import aiohttp
+from aiohttp import web
 import logging
+import sys
 from logging.handlers import RotatingFileHandler
+import json
 
 def create(whitelist="whitelist.txt", forward=8080, log="proxy.log", **kwargs):
-    app = Flask(__name__)
-    with open(whitelist) as f:
-        app.whitelist = set(line.rstrip() for line in f)
-    app.forward = forward
-
     handler = RotatingFileHandler(log, maxBytes=0x100000, backupCount=3)
-    handler.setLevel(logging.INFO)
     handler.setFormatter(RequestFormatter())
-    app.logger.addHandler(handler)
-    app.logger.setLevel(logging.INFO)
+    logger = logging.getLogger("Proxy")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    logger.addHandler(logging.StreamHandler(sys.stderr))
 
-    def error_msg(code, msg, id):
+    with open(whitelist) as f:
+        whitelist_set = set(line.rstrip() for line in f)
+        
+    def error_msg(code, msg, id, status):
         id = "null" if id is None else id
-        return '{{"jsonrpc":"2.0","error":{{"code":{},"message":"{}"}},"id":{}}}\n'.format(code, msg, id)
-
-    def parse_error():
-        return error_msg(-32700, "Parse error", None), 500
+        return web.json_response (
+            data={ "jsonrpc": "2.0", "error": {"code": code, "message": msg}, "id": id},
+            status=status
+        )
 
     def invalid_request():
-        return error_msg(-32600, "Invalid request", None), 400
+        return error_msg(-32600, "Invalid request", None, 400)
 
     def method_not_found(id):
-        return error_msg(-32601, "Method not found", id), 404
+        return error_msg(-32601, "Method not found", id, 404)
 
     def internal_error(id):
-        return error_msg(-32603, "Internal error", id), 500
+        return error_msg(-32603, "Internal error", id, 500)
 
-    @app.route('/', methods = ['POST'])
-    def proxy():
-        if not request.is_json:
-            app.logger.error('Received request is not JSON')
+    forward_addr = 'http://localhost:{}'.format(forward)
+
+    async def proxy(request):
+        try:
+            text = await request.text()
+            content = json.loads(text)
+        except:
+            logger.exception('Request is a invalid JSON')
             return invalid_request()
 
-        content = request.get_json(silent=True)
-        if content is None:
-            app.logger.error('Failed to parse JSON request: {}'.format(request.data))
-            return parse_error()
-
-        if 'id' not in content:
-            return ""
-
-        if 'method' in content and content['method'] in app.whitelist:
-            try:
-                forward_addr = 'http://localhost:{}'.format(app.forward)
-                r = requests.post(forward_addr, json=content)
-                app.logger.info('Successfully forwarded {} / Response {}'.format(content, r.text.strip()))
-                return r.text
-            except Exception as e:
-                app.logger.error('Failed to receive the response from the server: {}'.format(e))
-                return internal_error(content['id'])
-        elif 'method' in content:
-            app.logger.error('Filtered {}: {}'.format(content['method'], content))
+        if 'id' not in content or 'method' not in content:
+            return web.Response()
+        
+        if content['method'] not in whitelist_set:
+            logger.error('Filtered {}:{}}'.format(content['method'], text))
             return method_not_found(content['id'])
 
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(forward_addr, json=content) as resp:
+                    response = await resp.text()
+
+            logger.info('Successfully forwarded {} / Response {}'.format(text, response.strip()))
+            return web.Response(text=response, content_type='application/json')
+        except Exception as e:
+            logger.error('Failed to receive the response from the server: {}'.format(e))
+            return internal_error(content['id'])
+    
+    app = web.Application()
+    app.add_routes([web.post('/', proxy)])
     return app
 
 class RequestFormatter(logging.Formatter):
     def __init__(self, fmt=None, **kwargs):
-        default_fmt = '[%(asctime)s] [%(levelname)s in %(module)s] [%(remote_addr)s] %(message)s'
+        default_fmt = '[%(asctime)s] [%(levelname)s in %(module)s] %(message)s'
         if fmt is None:
             super().__init__(fmt=default_fmt, **kwargs)
         else:
             super().__init__(**kwargs)
 
     def format(self, record):
-        record.remote_addr = request.remote_addr
         return super(RequestFormatter, self).format(record)
 
 if __name__ == '__main__':
@@ -87,9 +89,7 @@ if __name__ == '__main__':
                         help='port to forward (deafult: 8080)')
     parser.add_argument('--log', default='proxy.log',
                         help='log file path (default: \"proxy.log\")')
-    parser.add_argument('--debug', action='store_true',
-                        help='enable debug mode')
 
     args = parser.parse_args()
     app = create(**vars(args))
-    app.run(host=args.bind, port=args.port, debug=args.debug)
+    web.run_app(app, host=args.bind, port=args.port)
